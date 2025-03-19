078dfcfa17421cb88880ffbad8e1ed24320c50da
mwshaner <mwshaner@gmail.com>
2025-03-19 14:34:01 -0400

    Moved the imgui window functions into their respective classes

--- src/View.cpp
+++ src/View.cpp
@@ -1,9 +1,9 @@
 #include "View.h"
 
 /*
-----------------------------------------------------------------
+-------------------------------------------------------------------------------------------------------------
                        LOGIN VIEW
-----------------------------------------------------------------
+-------------------------------------------------------------------------------------------------------------
 */
 
 
@@ -26,17 +26,153 @@ void LoginView::render()
 
     m_uiBackend.newUIFrame();
     m_uiBackend.prepareDockspace();
-    m_loginStatus = login();
+    m_loginStatus = loginWindow();
     m_uiBackend.renderUI();
 
+}
+
+bool LoginView::loginWindow()
+{
+	static std::string username(128, '\0');
+	static std::string password(128, '\0');
+	static std::string storedUsername;
+	static std::string storedPassword;
+	static std::string pass = "1";
+	static bool needsAccount = false;
+	bool result = false;
+	static bool showErr = false;
+
+
+	ImGui::Begin("##Login", nullptr, ImGuiWindowFlags_NoTitleBar);
+
+
+	ImGui::Dummy(ImVec2(0, 400));
+
+
+	// Choose the widget width (adjust as needed)
+	float widgetWidth = 800.0f;
+	float posX = (ImGui::GetWindowContentRegionWidth() - widgetWidth) * 0.5f;
+
+
+	// Center and draw the label
+	ImGui::SetCursorPosX(posX);
+	ImGui::Text("Username");
+	ImGui::Dummy(ImVec2(0, 5));
+	ImGui::SetCursorPosX(posX);
+	ImGui::PushItemWidth(widgetWidth);
+	ImGui::InputText("##Username", &username[0], username.size());
+	ImGui::PopItemWidth();
+
+	ImGui::Dummy(ImVec2(0, 25));
+
+	ImGui::SetCursorPosX(posX);
+	ImGui::Text("Password");
+	ImGui::Dummy(ImVec2(0, 5));
+	ImGui::SetCursorPosX(posX);
+	ImGui::PushItemWidth(widgetWidth);
+	ImGui::InputText("##Password", &password[0], password.size(), ImGuiInputTextFlags_Password);
+
+	ImGui::NewLine();
+	ImGui::NewLine();
+
+	if (ButtonCenteredOnLine("Login"))
+	{
+		storedUsername = username;
+		storedPassword = password;
+
+		storedPassword = storedPassword.c_str();
+		//showErr = storedPassword == pass ? true : false;
+		if (storedPassword != pass)
+		{
+			showErr = true;
+		}
+	}
+
+	if (ButtonCenteredOnLine("Create Account"))
+	{
+		needsAccount = true;
+	}
+
+	if (needsAccount)
+	{
+		result = createAccountWindow();
+
+		// Temporary to simulate behavior of sucessful account creation in the DB
+		needsAccount = !result;
+	}
+
+	if (showErr)
+	{
+		ImGui::PushStyleColor(ImGuiCol_Text, ImVec4(0.8f, 0.1f, 0.15f, 1.0f));
+		ImGui::SetCursorPosX(posX - 250);
+		ImGui::Text("Sorry, We Have No Account Associated With That Username/Password");
+		ImGui::PopStyleColor();
+	}
+
+	// Temporary until DB queries are fully incoroprated
+	if (storedPassword == pass)
+	{
+		ImGui::End();
+		return true;
+	}
+
+	ImGui::End();
+	return false;
+}
+
+bool LoginView::createAccountWindow()
+{
+	static std::string CAusername(128, '\0');
+	static std::string CApassword(128, '\0');
+	static std::string CAstoredUsername;
+	static std::string CAstoredPassword;
+
+	ImGui::Begin("Create Account");
+	ImGui::Dummy(ImVec2(0, 400));
+
+	// Widget width may need some experimentation depending on the your use case
+	float widgetWidth = 800.0f;
+	float posX = (ImGui::GetWindowContentRegionWidth() - widgetWidth) * 0.5f;
+
+
+	// Center and draw the label
+	ImGui::SetCursorPosX(posX);
+	ImGui::Text("Please Enter a Username");
+	ImGui::Dummy(ImVec2(0, 5));
+	ImGui::SetCursorPosX(posX);
+	ImGui::PushItemWidth(widgetWidth);
+	ImGui::InputText("##Username", &CAusername[0], CAusername.size());
+	ImGui::PopItemWidth();
+
+	ImGui::Dummy(ImVec2(0, 25));
+
+	ImGui::SetCursorPosX(posX);
+	ImGui::Text("Please Enter a Password");
+	ImGui::Dummy(ImVec2(0, 5));
+	ImGui::SetCursorPosX(posX);
+	ImGui::PushItemWidth(widgetWidth);
+	ImGui::InputText("##Password", &CApassword[0], CApassword.size(), ImGuiInputTextFlags_Password);
+
+	ImGui::NewLine();
+	ImGui::NewLine();
+
+	ImGui::SetCursorPosX(posX);
+	ImGui::Text("Press Submit to Create Your Account");
+	if (ButtonCenteredOnLine("Submit"))
+	{
+		ImGui::End();
+		return true;
+	}
+	ImGui::End();
+	return false;
 }
 
 
 
 /*
-----------------------------------------------------------------
+---------------------------------------------------------------------------------------------------------------------
                        MESH VIEW
-----------------------------------------------------------------
+---------------------------------------------------------------------------------------------------------------------
 */
 
 MeshView::MeshView(GLFWwindow* window, UI backend, std::vector<meshObject>& meshes)
@@ -54,30 +190,96 @@ void MeshView::render()
     m_uiBackend.setFont(std::string("RudaBold25"));
     m_uiBackend.newUIFrame();
     m_uiBackend.prepareDockspace();
-    sceneHierarchy(m_meshes);
-    MaterialEditor(m_meshes);
+    sceneHierarchyWindow();
+    materialEditorWindow();
     m_uiBackend.renderUI();
 }
 
-//bool MeshView::meshProperties()
-//{
-//    ImGui::Begin("Mesh Properties");
-//
-//    // Arrow buttons with Repeater
-//    static int counter = 0;
-//    float spacing = ImGui::GetStyle().ItemInnerSpacing.x;
-//    ImGui::PushButtonRepeat(true);
-//    if (ImGui::ArrowButton("##left", ImGuiDir_Left) && counter >= 0) { counter--; }
-//    ImGui::SameLine(0.0f, spacing);
-//    if (ImGui::ArrowButton("##right", ImGuiDir_Right) && counter < 10) { counter++; }
-//    ImGui::PopButtonRepeat();
-//    ImGui::SameLine();
-//    ImGui::Text("%d", counter);
-//
-//    ImGui::End();
-//}
+void MeshView::sceneHierarchyWindow()
+{
+	static glm::vec3& translation = m_meshes.at(0).m_mesh.getTranslation();
+	static glm::vec3& rotation = m_meshes.at(0).m_mesh.getRotation();
+	static glm::vec3& scale = m_meshes.at(0).m_mesh.getScale();
 
-//void MeshView::addObject(Mesh& obj)
-//{
-//    m_meshObjects.emplace_back(obj);
-//}
+	//static int selectedIndex = -1;
+
+	ImGui::Begin("Scene Hierarchy");
+	ImGui::SeparatorText("Objects");
+
+	ImVec2 availSize = ImGui::GetContentRegionAvail();
+
+	float topHeight = availSize.y * 0.5f;
+
+	ImGui::BeginChild("TopChild", ImVec2(0, topHeight), true);
+
+	for (size_t i = 0; i < m_meshes.size(); i++)
+	{
+		bool isSelected = (m_selectedIndex == i);
+		if (ImGui::Selectable(m_meshes.at(i).m_mesh.m_meshName.c_str(), isSelected))
+		{
+			m_selectedIndex = (m_selectedIndex == i) ? -1 : i;
+		}
+	}
+	ImGui::EndChild();
+
+	ImGui::BeginChild("Object Properties", ImVec2(0, 0), true);
+	ImGui::SeparatorText("Mesh Properties");
+	if (m_selectedIndex >= 0)
+	{
+		// Mesh Controls
+		drawVec3Control("Translation", &translation, 0.0, 120);
+		drawVec3Control("Rotation", &rotation, 1.0, 120);
+		drawVec3Control("Scale", &scale, 1.0, 120);
+
+		// Manipulates the mesh based on the controllers
+		m_meshes.at(m_selectedIndex).m_mesh.translateMesh(translation);
+		m_meshes.at(m_selectedIndex).m_mesh.rotateMesh(rotation);
+		m_meshes.at(m_selectedIndex).m_mesh.scaleMesh(scale);
+	}
+
+	ImGui::EndChild();
+	ImGui::End();
+}
+
+void MeshView::materialEditorWindow()
+{
+	ImGui::Begin("Material Editor");
+	ImGui::NewLine();
+
+	if (m_selectedIndex >= 0)
+	{
+		static glm::vec3& ambient = m_meshes.at(m_selectedIndex).m_mat.ambient;
+		static glm::vec3& specular = m_meshes.at(m_selectedIndex).m_mat.specular;
+		static glm::vec3& diffuse = m_meshes.at(m_selectedIndex).m_mat.diffuse;
+		static float& shininess = m_meshes.at(m_selectedIndex).m_mat.shininess;
+		static glm::vec2& uvScale = m_meshes.at(m_selectedIndex).m_mesh.m_UVScale;
+
+		ImGui::SeparatorText("Material Properties");
+
+		// Mesh Controls
+		drawVec3Control("Ambient", &ambient, 0.0, 120);
+		drawVec3Control("Specular", &specular, 0.0, 120);
+		drawVec3Control("Diffuse", &diffuse, 0.0, 120);
+
+		ImGui::Columns(2);
+		ImGui::SetColumnWidth(0, 120.0f);
+		ImGui::Text("Shininess");
+		ImGui::NextColumn();
+		ImGui::DragFloat("##Shininess", &shininess, 0.05);
+		ImGui::Columns(1);
+
+		ImGui::NewLine();
+		ImGui::SeparatorText("Textures");
+
+		/*for (int i = 0; i < 2; i++)
+		{
+			ImGui::NewLine();
+			ImGui::Image((ImTextureID)(intptr_t)meshes.at(i).m_mesh.texture.getTextures()[i], ImVec2(250, 250));
+		}*/
+		ImGui::Image((ImTextureID)(intptr_t)m_meshes.at(m_selectedIndex).m_mesh.m_texture.getTextures()[0], ImVec2(512, 512));
+
+		ImGui::NewLine();
+		drawVec2Control("UV Scale", &uvScale, 1.0f, 100.0f);
+	}
+	ImGui::End();
+}
--- src/View.h
+++ src/View.h
@@ -4,6 +4,8 @@
 #include "UI.h"
 //#include "Controller.h"
 
+
+// Utility functions for centering buttons and drawing widgets
 namespace 
 {
 	
@@ -23,7 +25,7 @@ namespace
 		return ImGui::Button(label);
 	}
 
-	static void drawVec3Control(const std::string& label, glm::vec3& values, float resetValue = 0.0f, float columnWidth = 100.0f)
+	static void drawVec3Control(const std::string& label, glm::vec3* values, float resetValue = 0.0f, float columnWidth = 100.0f)
 	{
 		ImGui::PushID(label.c_str());
 
@@ -43,10 +45,10 @@ namespace
 		ImGui::PushStyleColor(ImGuiCol_ButtonActive, ImVec4(0.8f, 0.1f, 0.15f, 1.0f));
 		if (ImGui::Button("X", buttonSize))
 		{
-			values.x = resetValue;
+			values->x = resetValue;
 		}
 		ImGui::SameLine();
-		ImGui::DragFloat("##X", &values.x, 0.07f);
+		ImGui::DragFloat("##X", &values->x, 0.07f);
 		ImGui::PopItemWidth();
 		ImGui::SameLine();
 		ImGui::PopStyleColor(3);
@@ -57,10 +59,10 @@ namespace
 		ImGui::PushStyleColor(ImGuiCol_ButtonActive, ImVec4(0.2f, 0.7f, 0.2f, 1.0f));
 		if (ImGui::Button("Y", buttonSize))
 		{
-			values.y = resetValue;
+			values->y = resetValue;
 		}
 		ImGui::SameLine();
-		ImGui::DragFloat("##Y", &values.y, 0.07f);
+		ImGui::DragFloat("##Y", &values->y, 0.07f);
 		ImGui::PopItemWidth();
 		ImGui::SameLine();
 		ImGui::PopStyleColor(3);
@@ -71,10 +73,10 @@ namespace
 		ImGui::PushStyleColor(ImGuiCol_ButtonActive, ImVec4(0.1f, 0.25f, 0.8f, 1.0f));
 		if (ImGui::Button("Z", buttonSize))
 		{
-			values.z = resetValue;
+			values->z = resetValue;
 		}
 		ImGui::SameLine();
-		ImGui::DragFloat("##Z", &values.z, 0.07f);
+		ImGui::DragFloat("##Z", &values->z, 0.07f);
 		ImGui::PopItemWidth();
 		ImGui::SameLine();
 		ImGui::PopStyleColor(3);
@@ -84,220 +86,51 @@ namespace
 		ImGui::PopID();
 	}
 
-	bool createAccount()
+	static void drawVec2Control(const std::string& label, glm::vec2* values, float resetValue = 0.0f, float columnWidth = 100.0f)
 	{
-		static std::string CAusername(128, '\0');
-		static std::string CApassword(128, '\0');
-		static std::string CAstoredUsername;
-		static std::string CAstoredPassword;
+		ImGui::PushID(label.c_str());
 
-		ImGui::Begin("Create Account");
+		ImGui::Columns(2);
+		ImGui::SetColumnWidth(0, columnWidth);
+		ImGui::Text(label.c_str());
+		ImGui::NextColumn();
 
+		ImGui::PushMultiItemsWidths(3, ImGui::CalcItemWidth());
+		ImGui::PushStyleVar(ImGuiStyleVar_ItemSpacing, ImVec2{ 0,0 });
 
-		ImGui::Dummy(ImVec2(0, 400));
+		float lineHeight = GImGui->Font->FontSize + GImGui->Style.FramePadding.y * 2.0f;
+		ImVec2 buttonSize = { lineHeight + 3.0f, lineHeight };
 
-
-		// Choose the widget width (adjust as needed)
-		float widgetWidth = 800.0f;
-		float posX = (ImGui::GetWindowContentRegionWidth() - widgetWidth) * 0.5f;
-
-
-		// Center and draw the label
-		ImGui::SetCursorPosX(posX);
-		ImGui::Text("Please Enter a Username");
-		ImGui::Dummy(ImVec2(0, 5));
-		ImGui::SetCursorPosX(posX);
-		ImGui::PushItemWidth(widgetWidth);
-		ImGui::InputText("##Username", &CAusername[0], CAusername.size());
+		ImGui::PushStyleColor(ImGuiCol_Button, ImVec4(0.8f, 0.1f, 0.15f, 1.0f));
+		ImGui::PushStyleColor(ImGuiCol_ButtonHovered, ImVec4(0.9f, 0.2f, 0.2f, 1.0f));
+		ImGui::PushStyleColor(ImGuiCol_ButtonActive, ImVec4(0.8f, 0.1f, 0.15f, 1.0f));
+		if (ImGui::Button("X", buttonSize))
+		{
+			values->x = resetValue;
+		}
+		ImGui::SameLine();
+		ImGui::DragFloat("##X", &values->x, 0.07f);
 		ImGui::PopItemWidth();
+		ImGui::SameLine();
+		ImGui::PopStyleColor(3);
 
-		ImGui::Dummy(ImVec2(0, 25));
 
-		ImGui::SetCursorPosX(posX);
-		ImGui::Text("Please Enter a Password");
-		ImGui::Dummy(ImVec2(0, 5));
-		ImGui::SetCursorPosX(posX);
-		ImGui::PushItemWidth(widgetWidth);
-		ImGui::InputText("##Password", &CApassword[0], CApassword.size(), ImGuiInputTextFlags_Password);
-
-		ImGui::NewLine();
-		ImGui::NewLine();
-
-		ImGui::SetCursorPosX(posX);
-		ImGui::Text("Press Submit to Create Your Account");
-		if (ButtonCenteredOnLine("Submit"))
-		{	
-			ImGui::End();
-			return true;	
+		ImGui::PushStyleColor(ImGuiCol_Button, ImVec4(0.2f, 0.7f, 0.3f, 1.0f));
+		ImGui::PushStyleColor(ImGuiCol_ButtonHovered, ImVec4(0.3f, 0.8f, 0.3f, 1.0f));
+		ImGui::PushStyleColor(ImGuiCol_ButtonActive, ImVec4(0.2f, 0.7f, 0.2f, 1.0f));
+		if (ImGui::Button("Y", buttonSize))
+		{
+			values->y = resetValue;
 		}
-		ImGui::End();
-		return false;
-	}
-
-	bool login()
-	{
-		static std::string username(128, '\0');
-		static std::string password(128, '\0');
-		static std::string storedUsername;
-		static std::string storedPassword;
-		static std::string pass = "1";
-		static bool createAccountWindow = false;
-		bool result = false;
-		static bool showErr = false;
-
-
-		ImGui::Begin("##Login", nullptr, ImGuiWindowFlags_NoTitleBar);
-		
-
-		ImGui::Dummy(ImVec2(0, 400));
-
-
-		// Choose the widget width (adjust as needed)
-		float widgetWidth = 800.0f;
-		float posX = (ImGui::GetWindowContentRegionWidth() - widgetWidth) * 0.5f;
-
-
-		// Center and draw the label
-		ImGui::SetCursorPosX(posX);
-		ImGui::Text("Username");
-		ImGui::Dummy(ImVec2(0, 5));
-		ImGui::SetCursorPosX(posX);
-		ImGui::PushItemWidth(widgetWidth);
-		ImGui::InputText("##Username", &username[0], username.size());
+		ImGui::SameLine();
+		ImGui::DragFloat("##Y", &values->y, 0.07f);
 		ImGui::PopItemWidth();
+		ImGui::SameLine();
+		ImGui::PopStyleColor(3);
 
-		ImGui::Dummy(ImVec2(0, 25));
-
-		ImGui::SetCursorPosX(posX);
-		ImGui::Text("Password");
-		ImGui::Dummy(ImVec2(0, 5));
-		ImGui::SetCursorPosX(posX);
-		ImGui::PushItemWidth(widgetWidth);
-		ImGui::InputText("##Password", &password[0], password.size(), ImGuiInputTextFlags_Password);
-		
-		ImGui::NewLine();
-		ImGui::NewLine();
-
-		if (ButtonCenteredOnLine("Login"))
-		{
-			storedUsername = username;
-			storedPassword = password;
-
-			storedPassword = storedPassword.c_str();
-			//showErr = storedPassword == pass ? true : false;
-			if (storedPassword != pass)
-			{
-				showErr = true;
-			}
-		}
-
-		if (ButtonCenteredOnLine("Create Account"))
-		{
-			createAccountWindow = true;
-		}
-
-		if (createAccountWindow)
-		{
-			result = createAccount();
-
-			// Temporary to simulate behavior of sucessful account creation in the DB
-			createAccountWindow = !result;
-		}
-
-		if (showErr)
-		{
-			ImGui::PushStyleColor(ImGuiCol_Text, ImVec4(0.8f, 0.1f, 0.15f, 1.0f));
-			ImGui::SetCursorPosX(posX - 250);
-			ImGui::Text("Sorry, We Have No Account Associated With That Username/Password");
-			ImGui::PopStyleColor();
-		}
-
-		// Temporary until DB queries are fully incoroprated
-		if (storedPassword == pass)
-		{
-			ImGui::End();
-			return true;
-		}
-
-		ImGui::End();
-		return false;
-	}
-
-	void sceneHierarchy(std::vector<meshObject>& meshes)
-	{
-		static glm::vec3 translation = meshes.at(0).m_mesh.getTranslation();
-		static glm::vec3 rotation = meshes.at(0).m_mesh.getRotation();
-		static glm::vec3 scale = meshes.at(0).m_mesh.getScale();
-
-		static int selectedIndex = -1;
-
-		ImGui::Begin("Scene Hierarchy");
-		ImGui::SeparatorText("Objects");
-
-		ImVec2 availSize = ImGui::GetContentRegionAvail();
-
-		float topHeight = availSize.y * 0.5f;
-
-		ImGui::BeginChild("TopChild", ImVec2(0, topHeight), true);
-		
-		for (size_t i = 0; i < meshes.size(); i++)
-		{
-			bool isSelected = (selectedIndex == i);
-			if (ImGui::Selectable(meshes.at(i).m_mesh.m_meshName.c_str(), isSelected))
-			{
-				selectedIndex = (selectedIndex == i) ? -1 : i;
-			}
-		}
-		ImGui::EndChild();
-
-		ImGui::BeginChild("Object Properties", ImVec2(0, 0), true);
-		ImGui::SeparatorText("Mesh Properties");
-		if (selectedIndex >= 0)
-		{
-			// Mesh Controls
-			drawVec3Control("Translation", translation, 0.0, 120);
-			drawVec3Control("Rotation", rotation, 1.0, 120);
-			drawVec3Control("Scale", scale, 1.0, 120);
-
-			// Manipulates the mesh based on the controllers
-			meshes.at(selectedIndex).m_mesh.translateMesh(translation);
-			meshes.at(selectedIndex).m_mesh.rotateMesh(rotation);
-			meshes.at(selectedIndex).m_mesh.scaleMesh(scale);
-		}
-
-		ImGui::EndChild();
-		ImGui::End();
-	}
-
-	void MaterialEditor(std::vector<meshObject>& meshes)
-	{
-		//Material sodaMat(glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.5f, 0.0f, 0.0f), glm::vec3(0.7f, 0.6f, 0.6f), 0.25f);
-		static glm::vec3 ambient(meshes.at(0).m_mat.ambient);
-		static glm::vec3 specular(meshes.at(0).m_mat.specular);
-		static glm::vec3 diffuse(meshes.at(0).m_mat.diffuse);
-		static glm::vec3 shininess(meshes.at(0).m_mat.shininess);
-
-		ImGui::Begin("Material Editor");
-		ImGui::NewLine();
-		ImGui::SeparatorText("Material Properties");
-
-
-		// Mesh Controls
-		drawVec3Control("Ambient", ambient, 0.0, 120);
-		drawVec3Control("Specular", specular, 0.0, 120);
-		drawVec3Control("Diffuse", diffuse, 0.0, 120);
-		drawVec3Control("Shininess", shininess, 0.0, 120);
-
-		ImGui::NewLine();
-		ImGui::SeparatorText("Textures");
-
-		/*for (int i = 0; i < 2; i++)
-		{
-			ImGui::NewLine();
-			ImGui::Image((ImTextureID)(intptr_t)meshes.at(i).m_mesh.texture.getTextures()[i], ImVec2(250, 250));
-		}*/
-		ImGui::Image((ImTextureID)(intptr_t)meshes.at(0).m_mesh.m_texture.getTextures()[0], ImVec2(512, 512));
-		ImGui::End();
+		ImGui::PopStyleVar();
+		ImGui::Columns(1);
+		ImGui::PopID();
 	}
 }
 
@@ -314,7 +147,8 @@ class LoginView
 public:
 	LoginView(GLFWwindow* window, UI backend);
 	void render();
-	void loginWindow();
+	bool loginWindow();
+	bool createAccountWindow();
 	bool getLoginStatus() { return m_loginStatus; }
 
 private:
@@ -330,6 +164,8 @@ class MeshView
 public:
 	MeshView(GLFWwindow* window, UI backend, std::vector<meshObject>& meshes);
 	void render();
+	void sceneHierarchyWindow();
+	void materialEditorWindow();
 	//bool meshProperties();
 	//void addObject(Mesh& obj);
 
@@ -339,4 +175,5 @@ private:
 	GLFWwindow* m_window;
 	ImGuiIO& m_io;
 	std::vector<meshObject>& m_meshes;
+	int16_t m_selectedIndex = -1;
 };
